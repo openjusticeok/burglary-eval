c4ee39d0cb4ad0afc41e793cd8b07d4040f7ac16
Ryan Gentzler <rgentzler@okpolicy.org>
2020-06-23 13:11:12 -0500

    feedback 6/23

--- R script ryan.R
+++ R script ryan.R
@@ -30,7 +30,7 @@ casesall19 %>% # get a list of all unique values of top_ct_desc
   distinct() %>%
   view
 
-?str_detect 
+?str_detect
 
 burgall19 <- casesall19 %>%
   mutate(burglary = str_detect(top_ct_desc, "BURGLARY") & !str_detect(top_ct_desc, "TOOL")) %>%
@@ -39,7 +39,7 @@ burgall19 <- casesall19 %>%
 #count filings by month
 burg_sum19 <- burgall19 %>%
   mutate(file_month = floor_date(ymd(file_date), 'month')) %>%
-  count(file_month) 
+  count(file_month)
 
 #Add court county-- this doesn't graph right, need to get each county own line
 
@@ -79,7 +79,7 @@ ggplot(burg20_sum, aes(file_month, n, color = 'court')) +
   ggtitle("Number of Burglary Cases Filed in OSCN Counties 2020") +
   scale_color_manual(values = ojo_pal)
 
-#Note: Cases dropped after March, COVID probably 
+#Note: Cases dropped after March, COVID probably
 
 #Just first degree Burg
 
@@ -147,7 +147,6 @@ ggplot(burg_sum2nd19, aes(file_month, n, color = 'court')) +
   ggtitle("Number of 2nd Burglary Cases Filed in OSCN Counties 2019") +
   scale_color_manual(values = ojo_pal)
 
-
 #Just 3rd Degree
 
 burg3rd19 <- casesall19 %>%
@@ -178,6 +177,40 @@ ggplot(burg_sum3rd20, aes(file_month, n, color = 'court')) +
   theme_ojo() +
   ylim(0, NA) +
   ggtitle("Number of 3rd Degree Burglary Cases Filed in OSCN Counties 2020") +
+  scale_color_manual(values = ojo_pal)
+
+
+### This is great work! To shorten this iteration, we can query both years at the same time: ####
+
+casesall <- ojo_tbl('ojo_crim_cases') %>%
+  filter(court %in% c("ADAIR", "CANADIAN", "CLEVELAND", "COMANCHE", "ELLIS", "GARFIELD", "LOGAN", "OKLAHOMA", "PAYNE", "PUSHMATAHA","ROGERMILLS", "ROGERS", "TULSA"), file_year >= 2019, casetype == "CF") %>%
+  collect()
+
+### Same as your code, but for both years
+burgall <- casesall %>%
+  mutate(burglary = str_detect(top_ct_desc, "BURGLARY") & !str_detect(top_ct_desc, "TOOL")) %>%
+  filter(burglary == TRUE)
+
+### Now, instead of creating a new dataframe for each category of burglary, we can create a new variable that tells us which one it is. Look at the ?case_when function - super useful in these situations. It's basically a long if-then statement. If you detect "FIRST" in top_ct_desc, then put "FIRST DEGREE". If not, then look for "SECOND", etc.
+
+burgall <- burgall %>%
+  mutate(burg_cat = case_when(str_detect(top_ct_desc, "FIRST|1") ~ "FIRST DEGREE",
+                              str_detect(top_ct_desc, "SECOND|2") ~ "SECOND DEGREE",
+                              str_detect(top_ct_desc, "THIRD|3") ~ "THIRD DEGREE"),
+         file_month = floor_date(ymd(file_date), 'month'))
+
+## Now we can summarize them all into one dataframe, avoiding all the repetition above. One of the best habits you can get in is recognizing when you're repeating things a lot and think about how you can let the machine do that work for you. Takes a lot of practice, but very worth it!
+
+burg_sum <- burgall %>%
+  count(file_month, burg_cat) # Counting two variables looks for all unique combinations - look at burg_sum to see what it does
+
+## Plot them all next to each other - makes it easier to see how they interact
+
+ggplot(burg_sum, aes(file_month, n, group = burg_cat, color = burg_cat)) +
+  geom_line() +
+  theme_ojo() +
+  ylim(0, NA) +
+  ggtitle("Number of Burglary Cases Filed in OSCN Counties") +
   scale_color_manual(values = ojo_pal)
 
 #Possession with intent to distribute (NOTE TO RYAN-- I'm GOING TO LOOK THROUGH LATER, BUT DO YOU THINK MY STR_DETECT TERMS ARE OK?)
@@ -212,7 +245,7 @@ ggplot(PWI_sum19, aes(file_month, n, color = 'court')) +
   ggtitle("Number of PWI Cases Filed in OSCN Counties 2019") +
   scale_color_manual(values = ojo_pal)
 
-#Same but for dispositions: 
+#Same but for dispositions:
 
 #This is the variable for disps in ALL OSCN counties
 
@@ -230,7 +263,7 @@ burgdisp19 <- dispsall19 %>%
 #disps by month
 burg_dispsum19 <- burgdisp19 %>%
   mutate(file_month = floor_date(ymd(disp_date), 'month')) %>%
-  count(file_month) 
+  count(file_month)
 
 #Add court county-- this doesn't graph right, need to get each county own line
 
@@ -253,11 +286,11 @@ ggplot(burg_dispsum19, aes(file_month, n, color = 'court')) +
 
 dispsall20 <- ojo_tbl('oscn_crim_disps') %>%
   filter(court %in% c("ADAIR", "CANADIAN", "CLEVELAND", "COMANCHE", "ELLIS", "GARFIELD", "LOGAN", "OKLAHOMA", "PAYNE", "PUSHMATAHA","ROGERMILLS", "ROGERS", "TULSA"), file_year == 2020, casetype == "CF") %>%
-  collect() 
+  collect()
 
 burgdisp20 <- dispsall20 %>%
   mutate(burglary = str_detect(disp_desc, "BURGLARY") & !str_detect(disp_desc, "TOOL")) %>%
-  filter(burglary == TRUE) 
+  filter(burglary == TRUE)
 
 burgdisp20_sum <- burgdisp20 %>%
   mutate(file_month = floor_date(ymd(disp_date), 'month')) %>%
@@ -270,7 +303,7 @@ ggplot(burgdisp20_sum, aes(file_month, n, color = 'court')) +
   ggtitle("Number of Burglary Disps in OSCN Counties 2020") +
   scale_color_manual(values = ojo_pal)
 
-#Note: Cases dropped after March, COVID probably 
+#Note: Cases dropped after March, COVID probably
 
 #Just first degree Burg
 
@@ -372,7 +405,7 @@ ggplot(burg_dispsum3rd20, aes(file_month, n, color = 'court')) +
 #Add PWI disps next
 
 #LATER-- Comparing top with bottom income counties
-#This code is not finished 
+#This code is not finished
 
 casescan19 <- ojo_tbl('ojo_crim_cases') %>%
   filter(court == "CANADIAN", file_year == 2019, casetype == "CF") %>%
@@ -380,7 +413,7 @@ casescan19 <- ojo_tbl('ojo_crim_cases') %>%
 
 casescan20 <- ojo_tbl('ojo_crim_cases') %>%
   filter(court == "CANADIAN", file_year == 2020, casetype == "CF") %>%
-  collect() 
+  collect()
 
 burg2ndcomp19 <- casescomp19 %>%
   mutate(burglary = str_detect(top_ct_desc, "BURGLARY - SECOND DEGREE") & !str_detect(top_ct_desc, "TOOL")) %>%
